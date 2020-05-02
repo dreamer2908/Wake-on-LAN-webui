e282fa918652b6d281eb1a795830bb95d2e68873
dreamer2908 <dreamer2908@gmail.com>
2020-05-02 15:49:00 +0700

    Add a secondary ping api

---
+++ WebApplication1/Controllers/Ping2Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class Ping2Controller : ApiController
+    {
+        public int Get(string ip)
+        {
+            var pingResult = common.singlePing(ip, 5000, 32, 128, out string replyAddr, out long replyTime, out int replyTtl);
+            return (pingResult == common.pingStatus.online) ? 0 : 1;
+        }
+    }
+}
